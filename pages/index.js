48c5c80e3d25425138f7b840ea087d9d041cd8de
RockStone <RockStoneStudios666@gmail.com>
2022-01-18 18:17:08 -0500

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
          
        <title>M.O</title>
        <meta name="description" content="Gestion Seguridad y Salud en el trabajo" />
-       <script src="https://kit.fontawesome.com/6df470b726.js" crossorigin="anonymous"></script>
-       <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&family=Poppins:ital,wght@0,300;0,400;0,800;1,300;1,700&family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+       {/* <script src="https://kit.fontawesome.com/6df470b726.js" crossorigin="anonymous"></script> */}
+      
        
        <link rel="icon" href="/favicon.ico" />
       </Head>
