c8003f3d6cba6abe395ecae786969b53efd0a420
RockStone <RockStoneStudios666@gmail.com>
2022-02-26 09:47:08 -0500

    En Progreso . . .

--- components/ListBlog.jsx
+++ components/ListBlog.jsx
@@ -1,17 +1,22 @@
 import styles from '../styles/ListBlog.module.css';
 import Blog from './Blog';
+import Blog1 from './Blog1';
+import Blog2 from './Blog2';
+import Blog3 from './Blog3';
+import Blog4 from './Blog4';
+import Blog5 from './Blog5';
 const ListBlog = () => {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Articulos de Interes</h1>
-             <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ex accusamus, animi eveniet amet est! Exercitationem dicta placeat deleniti corporis architecto! Ea illum aut ut? Aperiam nihil inventore quae amet, optio, deserunt maxime numquam sit eius accusantium dolorem, non eum.</p>
+             <p className={styles.desc}>La Seguridad y Salud en el Trabajo ha sido tema de interes y en los sigientes link encontraras actualizaciones que ha menudo el Ministerio de Trabajo, las Administraciones de Riesgos laborales ARL  y el Ministerio de Salud y Proteccion Social comparten con el fin de continuar con la promocion y prevencion de los empleadores y colaboradores de la empresa</p>
             <div className={styles.wrapper}>
                 <Blog/>
-                <Blog/>
-                <Blog/>
-                <Blog/>
-                <Blog/>
-                <Blog/>
+                <Blog1/>
+                <Blog2/>
+                <Blog3/>
+                <Blog4/>
+                <Blog5/>
                 
 
             </div>
