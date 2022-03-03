b03afd7a589d1814cb2078828f52652518047545
RockStone <RockStoneStudios666@gmail.com>
2022-03-03 17:46:22 -0500

    Responsive with logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,13 +2,16 @@ import {useState} from 'react'
 import Link from 'next/link';
 import {FaTimes,FaBars} from 'react-icons/fa';
 import styles from '../styles/Navbar.module.css';
+import Image from 'next/image';
 
 const Navbar = () => {
   const [open,setOpen] = useState(false);
   return (
     <>
       <div className={styles.menu}>
-        <label className={styles.logo} >M.O</label>
+        <label className={styles.logo} >
+          <Image src= "/img/logo22.png" width={100} height={92}/>
+          </label>
           <ul className={open ?[styles.menu_items,styles.show].join(' ') : styles.menu_items}>
             <li onClick={()=>setOpen(false)}><Link href="/"><a>Home</a></Link></li>
             <li  onClick={()=>setOpen(false)}><Link href="/valores"><a>Valores</a></Link></li>
